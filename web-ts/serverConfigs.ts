93c7f9d1d47acae73f21a1f494855f0cbcf1ad0b
Leonard Shen <uvstudio2199@gmail.com>
2021-07-03 07:18:22 -0400

    test

--- web-ts/serverConfigs.ts
+++
@@ -1,7 +0,0 @@
-export const CURRENT_IP: string = 'http://192.168.0.198:5000';
-
-// AWS_ROUTE_IP=https://www.getdinge.com
-// HOME_IP=http://192.168.0.198:5000
-// AWS_IP=http://3.142.187.42
-// ELASTIC_IP=http://3.138.87.73
-// GETDINGE=https://www.getdinge.com
